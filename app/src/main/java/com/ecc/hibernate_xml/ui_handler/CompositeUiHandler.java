5935f66df2081800f2d3c82fc82520d11c571995
Emmett Young <emmettyoung92@gmail.com>
2017-10-02 10:19:48 +0800

    Removes the option of enabling/disabling going up the UiHandler hierarchy in ComposoteUiHandler.

--- app/src/main/java/com/ecc/hibernate_xml/ui_handler/CompositeUiHandler.java
+++ app/src/main/java/com/ecc/hibernate_xml/ui_handler/CompositeUiHandler.java
@@ -8,20 +8,16 @@ import com.ecc.hibernate_xml.util.InputHandler;
 public class CompositeUiHandler extends UiHandler {
 
 	private static String MAIN_PROMPT = "Please choose what operation you want to perform:\n%sOperation: ";
-	private static String BACK_OPTION = "0. Back\n";
+	private static String BACK_OPTION = "0. Close Menu\n";
 
-	private Boolean isBackOptionEnabled;
 	private List<UiHandler> uiHandlers;
 
 	public CompositeUiHandler() {
-		super(null);
-		this.isBackOptionEnabled = false;		
-		this.uiHandlers = new ArrayList<>();
+		this(null);
 	}
 
 	public CompositeUiHandler(String operationName) {
-		super(operationName);
-		this.isBackOptionEnabled = true;		
+		super(operationName);	
 		this.uiHandlers = new ArrayList<>();
 	}
 
@@ -31,23 +27,21 @@ public class CompositeUiHandler extends UiHandler {
 	}
 
 	@Override
-	public void handle() throws Exception {
-		String userPrompt = String.format(MAIN_PROMPT, buildOperationPrompt());
-		Integer optionIndex = InputHandler.getNextLine(userPrompt, 
-			input -> Integer.valueOf(input));
-
-		if (optionIndex > 0 || !isBackOptionEnabled) {
-			uiHandlers.get(optionIndex - 1).handle();
-			handle();
+	public void onHandle() throws Exception {
+		try {
+			String userPrompt = String.format(MAIN_PROMPT, buildOperationPrompt());
+			Integer optionIndex = InputHandler.getNextLine(userPrompt, Integer::valueOf);
+			if (optionIndex > 0) {
+				uiHandlers.get(optionIndex - 1).handle();
+			}
+		}
+		catch (Exception exception) {
+			throw new InputException(exception);
 		}
 	}
 
 	private String buildOperationPrompt() {		
-		StringBuilder builder = new StringBuilder();
-
-		if (isBackOptionEnabled) {
-			builder.append(BACK_OPTION);
-		}
+		StringBuilder builder = new StringBuilder(BACK_OPTION);
 
 		for (int i = 0; i < uiHandlers.size(); i++) {
 			builder.append(String.format("%d. %s\n", i + 1, uiHandlers.get(i).getOperationName()));
