984c57bf97ad92ac15377fbe3f7a4219294eaf48
Emmett Young <emmettyoung92@gmail.com>
2017-10-03 10:54:53 +0800

    Overrides the equals and hashCode in Role.

--- core/src/main/java/com/ecc/hibernate_xml/model/Role.java
+++ core/src/main/java/com/ecc/hibernate_xml/model/Role.java
@@ -29,6 +29,20 @@ public class Role {
 	}
 
 	@Override
+	public int hashCode() {
+		return id.hashCode() << 4 + name.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object != null && object instanceof Role) {
+			Role otherRole = (Role) object;
+			return otherRole.id.equals(id) && otherRole.name.equals(name);
+		}
+		return false;
+	}
+
+	@Override
 	public String toString() {
 		return String.format("------ ID: %d ------\nName: %s", id, name);
 	}
