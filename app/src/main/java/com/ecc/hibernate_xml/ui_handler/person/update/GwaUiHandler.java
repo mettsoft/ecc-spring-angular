22c36ce49b2d6ee5e946f66ad021de0e57a410e1
Emmett Young <emmettyoung92@gmail.com>
2017-10-03 11:21:38 +0800

    Adds GwaUiHandler.

---
+++ app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/update/GwaUiHandler.java
@@ -0,0 +1,36 @@
+package com.ecc.hibernate_xml.ui_handler.person.update;
+
+import java.math.BigDecimal;
+
+import com.ecc.hibernate_xml.ui_handler.CompositeUiHandler;
+import com.ecc.hibernate_xml.ui_handler.UiHandler;
+import com.ecc.hibernate_xml.util.InputHandler;
+import com.ecc.hibernate_xml.service.PersonService;
+import com.ecc.hibernate_xml.model.Person;
+
+public class GwaUiHandler extends UiHandler {
+
+	private static final String PROMPT = "Please enter the GWA: ";
+
+	private PersonService personService;
+	private Person person;
+
+	public GwaUiHandler(String operationName, Person person) {
+		super(operationName);
+		this.personService = new PersonService();
+		this.person = person;
+	}
+
+	@Override 
+	public void onHandle() throws Exception {
+		BigDecimal GWA = InputHandler.getNextLine(PROMPT, BigDecimal::new);
+		person.setGWA(GWA);
+		personService.updatePerson(person);
+		System.out.println(String.format("Successfully updated person's gwa GWA \"%s\"!", GWA));
+	}
+
+	@Override 
+	protected Boolean relinquishControl() {
+		return true;
+	}
+}
