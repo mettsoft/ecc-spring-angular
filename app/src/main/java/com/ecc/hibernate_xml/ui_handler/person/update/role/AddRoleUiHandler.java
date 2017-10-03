ba5af2d5cb203771a9049724ea445873162d8b9c
Emmett Young <emmettyoung92@gmail.com>
2017-10-03 11:22:53 +0800

    Adds AddRoleUiHandler.

---
+++ app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/update/role/AddRoleUiHandler.java
@@ -0,0 +1,46 @@
+package com.ecc.hibernate_xml.ui_handler.person.update.role;
+
+import java.util.stream.Collectors;
+import com.ecc.hibernate_xml.ui_handler.CompositeUiHandler;
+import com.ecc.hibernate_xml.ui_handler.UiHandler;
+import com.ecc.hibernate_xml.util.InputHandler;
+import com.ecc.hibernate_xml.service.PersonService;
+import com.ecc.hibernate_xml.service.RoleService;
+import com.ecc.hibernate_xml.model.Person;
+import com.ecc.hibernate_xml.model.Role;
+
+public class AddRoleUiHandler extends UiHandler {
+
+	private static final String PROMPT = "Please choose role from the following: ";
+
+	private PersonService personService;
+	private RoleService roleService;
+	private Person person;
+
+	public AddRoleUiHandler(String operationName, Person person) {
+		super(operationName);
+		this.personService = new PersonService();
+		this.roleService = new RoleService();
+		this.person = person;
+	}
+
+	@Override 
+	public void onHandle() throws Exception {
+
+		String listOfRoles = roleService.listRoles().stream()
+			.map(role -> String.format("[ID=%d] %s", role.getId(), role.getName()))
+			.collect(Collectors.joining("\n"));
+
+		Integer roleId = InputHandler.getNextLine(
+			String.format("%s\n%s\nRole ID: ", PROMPT, listOfRoles), Integer::valueOf);
+
+		personService.addRoleToPerson(roleId, person);
+		System.out.println(String.format(
+			"Successfully added role ID \"%d\" to Person \"%s\"!", roleId, person.getName()));
+	}
+
+	@Override 
+	protected Boolean relinquishControl() {
+		return true;
+	}
+}
