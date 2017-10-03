503c82c4dbe1a8d5fd4191049de21d56f3e13517
Emmett Young <emmettyoung92@gmail.com>
2017-10-03 11:24:12 +0800

    Adds List/Add/Remove roles in UpdatePersonUiHandler.

--- app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/UpdatePersonUiHandler.java
+++ app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/UpdatePersonUiHandler.java
@@ -7,6 +7,9 @@ import com.ecc.hibernate_xml.ui_handler.person.update.AddressUiHandler;
 import com.ecc.hibernate_xml.ui_handler.person.update.BirthdayUiHandler;
 import com.ecc.hibernate_xml.ui_handler.person.update.GwaUiHandler;
 import com.ecc.hibernate_xml.ui_handler.person.update.EmploymentUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.role.ListRolesUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.role.AddRoleUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.role.RemoveRoleUiHandler;
 import com.ecc.hibernate_xml.util.InputHandler;
 import com.ecc.hibernate_xml.service.PersonService;
 import com.ecc.hibernate_xml.model.Person;
@@ -29,13 +32,16 @@ public class UpdatePersonUiHandler extends UiHandler {
 		Integer personId = InputHandler.getNextLine(PROMPT, Integer::valueOf);
 		Person person = personService.getPerson(personId);
 		// TODO: Manage Contacts
-		// TODO: Manage Roles
 		new CompositeUiHandler()
 			.add(new NameUiHandler("Change name.", person))
 			.add(new AddressUiHandler("Change address.", person))
 			.add(new BirthdayUiHandler("Change birthday.", person))
 			.add(new GwaUiHandler("Change GWA", person))
 			.add(new EmploymentUiHandler("Change employment status.", person))
+			.add(new CompositeUiHandler("Manage Roles")
+				.add(new ListRolesUiHandler("List Roles", person))
+				.add(new AddRoleUiHandler("Add Role", person))
+				.add(new RemoveRoleUiHandler("Remove Role", person)))
 			.handle();
 	}
 
