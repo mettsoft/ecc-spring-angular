bfac7cbc5a5f17acaafe58f2d41ca1f9c66fab66
Emmett Young <emmettyoung92@gmail.com>
2017-10-03 13:40:08 +0800

    Integrated Contacts CRUD to UpdatePersonUiHandler.

--- app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/UpdatePersonUiHandler.java
+++ app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/UpdatePersonUiHandler.java
@@ -10,6 +10,10 @@ import com.ecc.hibernate_xml.ui_handler.person.update.EmploymentUiHandler;
 import com.ecc.hibernate_xml.ui_handler.person.update.role.ListRolesUiHandler;
 import com.ecc.hibernate_xml.ui_handler.person.update.role.AddRoleUiHandler;
 import com.ecc.hibernate_xml.ui_handler.person.update.role.RemoveRoleUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.contact.ListContactsUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.contact.AddContactUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.contact.EditContactUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.update.contact.DeleteContactUiHandler;
 import com.ecc.hibernate_xml.util.InputHandler;
 import com.ecc.hibernate_xml.service.PersonService;
 import com.ecc.hibernate_xml.model.Person;
@@ -38,10 +42,15 @@ public class UpdatePersonUiHandler extends UiHandler {
 			.add(new BirthdayUiHandler("Change birthday.", person))
 			.add(new GwaUiHandler("Change GWA", person))
 			.add(new EmploymentUiHandler("Change employment status.", person))
-			.add(new CompositeUiHandler("Manage Roles")
-				.add(new ListRolesUiHandler("List Roles", person))
-				.add(new AddRoleUiHandler("Add Role", person))
-				.add(new RemoveRoleUiHandler("Remove Role", person)))
+			.add(new CompositeUiHandler("Manage Contact Information.")
+				.add(new ListContactsUiHandler("List Contact Information.", person))
+				.add(new AddContactUiHandler("Add Contact Information.", person))
+				.add(new EditContactUiHandler("Edit Contact Information.", person))
+				.add(new DeleteContactUiHandler("Delete Contact Information", person)))
+			.add(new CompositeUiHandler("Manage Roles.")
+				.add(new ListRolesUiHandler("List Roles.", person))
+				.add(new AddRoleUiHandler("Add Role.", person))
+				.add(new RemoveRoleUiHandler("Remove Role.", person)))
 			.handle();
 	}
 
