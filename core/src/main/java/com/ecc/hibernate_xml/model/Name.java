7d4300973b18c93ebff5d7203e082655a4e84611
Emmett Young <emmettyoung92@gmail.com>
2017-10-04 14:32:20 +0800

    Adds validation rules to Name.

--- core/src/main/java/com/ecc/hibernate_xml/model/Name.java
+++ core/src/main/java/com/ecc/hibernate_xml/model/Name.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class Name {
+	private static Integer MAX_CHARACTERS = 20;
+
 	private String title;
 	private String lastName;
 	private String firstName;
@@ -22,22 +24,51 @@ public class Name {
 	}
 
 	public void setTitle(String title) {
+		if (title.length() > MAX_CHARACTERS) {
+			throw new ModelException(String.format("Title must not exceed %d characters.", 
+				MAX_CHARACTERS));
+		}
 		this.title = title;
 	}
 
-	public void setLastName(String lastName) {
+	public void setLastName(String lastName) throws ModelException {
+		if (lastName == null || lastName.trim().isEmpty()) {
+			throw new ModelException("Last name cannot be empty.");
+		}
+		else if (lastName.length() > MAX_CHARACTERS) {
+			throw new ModelException(String.format("Last name must not exceed %d characters.", 
+				MAX_CHARACTERS));
+		}
 		this.lastName = lastName;
 	}
 
 	public void setFirstName(String firstName) {
+		if (firstName == null || firstName.trim().isEmpty()) {
+			throw new ModelException("First name cannot be empty.");
+		}
+		else if (firstName.length() > MAX_CHARACTERS) {
+			throw new ModelException(String.format("First name must not exceed %d characters.", 
+				MAX_CHARACTERS));
+		}
 		this.firstName = firstName;
 	}
 
 	public void setMiddleName(String middleName) {
+		if (middleName == null || middleName.trim().isEmpty()) {
+			throw new ModelException("Middle name cannot be empty.");
+		}
+		else if (middleName.length() > MAX_CHARACTERS) {
+			throw new ModelException(String.format("Middle name must not exceed %d characters.", 
+				MAX_CHARACTERS));
+		}
 		this.middleName = middleName;
 	}
 
 	public void setSuffix(String suffix) {
+		if (suffix.length() > MAX_CHARACTERS) {
+			throw new ModelException(String.format("Suffix must not exceed %d characters.", 
+				MAX_CHARACTERS));
+		}
 		this.suffix = suffix;
 	}
 
