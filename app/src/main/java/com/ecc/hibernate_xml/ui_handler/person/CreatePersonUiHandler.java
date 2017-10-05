1eadc76595743fdf81c1dfbbf417eb2794a53650
Emmett Young <emmettyoung92@gmail.com>
2017-10-05 13:50:51 +0800

    Adds all modification in Create Person module.

--- app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/CreatePersonUiHandler.java
+++ app/src/main/java/com/ecc/hibernate_xml/ui_handler/person/CreatePersonUiHandler.java
@@ -1,6 +1,19 @@
 package com.ecc.hibernate_xml.ui_handler.person;
 
 import com.ecc.hibernate_xml.ui_handler.UiHandler;
+import com.ecc.hibernate_xml.ui_handler.CompositeUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.NameUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.AddressUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.BirthdayUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.GwaUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.EmploymentUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.contact.ListContactsUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.contact.AddContactUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.contact.EditContactUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.contact.DeleteContactUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.role.ListRolesUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.role.AddRoleUiHandler;
+import com.ecc.hibernate_xml.ui_handler.person.modify.role.RemoveRoleUiHandler;
 import com.ecc.hibernate_xml.util.InputHandler;
 import com.ecc.hibernate_xml.service.PersonService;
 import com.ecc.hibernate_xml.model.Person;
@@ -28,6 +41,24 @@ public class CreatePersonUiHandler extends UiHandler {
 
 		Person person = new Person(name);
 		personService.createPerson(person);
+
+		new CompositeUiHandler()
+			.add(new NameUiHandler("Change Name.", person))
+			.add(new AddressUiHandler("Change Address.", person))
+			.add(new BirthdayUiHandler("Change Birthday.", person))
+			.add(new GwaUiHandler("Change GWA", person))
+			.add(new EmploymentUiHandler("Change Employment Status.", person))
+			.add(new CompositeUiHandler("Manage Contact Information.")
+				.add(new ListContactsUiHandler("List Contact Information.", person))
+				.add(new AddContactUiHandler("Add Contact Information.", person))
+				.add(new EditContactUiHandler("Edit Contact Information.", person))
+				.add(new DeleteContactUiHandler("Delete Contact Information", person)))
+			.add(new CompositeUiHandler("Manage Roles.")
+				.add(new ListRolesUiHandler("List Roles.", person))
+				.add(new AddRoleUiHandler("Add Role.", person))
+				.add(new RemoveRoleUiHandler("Remove Role.", person)))
+			.handle();
+
 		System.out.println(String.format("Successfully created the person \"%s\" with ID \"%d\"!", 
 			person.getName(), person.getId()));
 	}
