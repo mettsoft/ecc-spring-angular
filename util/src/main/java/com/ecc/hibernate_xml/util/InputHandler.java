2fc7f5ef8e08df3e9f2fc2d266670261584cfad7
Emmett Young <emmettyoung92@gmail.com>
2017-10-12 14:45:56 +0800

    Removes unused method in InputHandler.

--- util/src/main/java/com/ecc/hibernate_xml/util/InputHandler.java
+++ util/src/main/java/com/ecc/hibernate_xml/util/InputHandler.java
@@ -6,11 +6,6 @@ import java.util.Scanner;
 public class InputHandler {
 	private static final Scanner SCANNER = new Scanner(System.in);
 
-	public static String getNextLine(String message) {
-		System.out.print(message);
-		return SCANNER.nextLine();		
-	}
-
 	public static <R> R getNextLine(String message, CheckedFunction<String, R> function) throws InputException {
 		System.out.print(message);
 		try {
