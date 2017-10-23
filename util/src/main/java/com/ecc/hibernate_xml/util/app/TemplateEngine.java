f68491a71c214755f05eaab6b81bef10a9809f99
Emmett Young <emmettyoung92@gmail.com>
2017-10-23 17:44:18 +0800

    Completes the renderTable method in TemplateEngine.

--- util/src/main/java/com/ecc/hibernate_xml/util/app/TemplateEngine.java
+++ util/src/main/java/com/ecc/hibernate_xml/util/app/TemplateEngine.java
@@ -1,14 +1,17 @@
-package com.ecc.hibernate_xml.util;
+package com.ecc.hibernate_xml.util.app;
 
 import java.io.PrintWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Map;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
 public class TemplateEngine {
+	private static final String UPDATE_BUTTON_FORM = "<form action=\"%s\" method=\"GET\"><input type=\"hidden\" name=\"id\" value=\"%s\"><button>Edit</button></form>";
+	private static final String DELETE_BUTTON_FORM = "<form action=\"%s\" method=\"POST\"><input type=\"hidden\" name=\"id\" value=\"%s\"><input type=\"hidden\" name=\"mode\" value=\"3\"><button onClick=\"return confirm('Are you sure you want to delete this entry?')\">Delete</button></form>";
 	private PrintWriter printWriter;
 
 	public TemplateEngine(PrintWriter printWriter) {
@@ -24,8 +27,29 @@ public class TemplateEngine {
 		printWriter.println(buffer);
 	}
 
-	// Stub
-	public String renderTable() {
-		return "<table>			<thead>				<th>ID</th>				<th>Role</th>			</thead>			<tbody>				<td>1</td>				<td>Accountant</td>			</tbody>		</table>";
+	public String renderTable(List<String> headers, List<List<String>> data, String editAction, String deleteAction) {
+		if (data.size() == 0) {
+			return "<h6>No records found!</h6>";
+		}
+
+		StringBuilder builder = new StringBuilder("<table><thead>");
+		for (String header: headers) {
+			builder.append(String.format("<th>%s</th>", header));
+		}
+		builder.append("<th></th><th></th>");
+		builder.append("</thead><tbody>");
+
+		for (List<String> row: data) {
+			builder.append("<tr>");
+			for (String column: row) {
+				builder.append(String.format("<td>%s</td>", column));
+			}
+			String updateButton = String.format(UPDATE_BUTTON_FORM, editAction, row.get(0));
+			String deleteButton = String.format(DELETE_BUTTON_FORM, deleteAction, row.get(0));
+			builder.append(String.format("<td>%s</td><td>%s</td></tr>", updateButton, deleteButton));
+		}
+		builder.append("</tbody></table>");
+
+		return builder.toString();
 	}
 }
