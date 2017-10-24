feabbb0a7611f56b669fd98bcd60b9773fd28550
Emmett Young <emmettyoung92@gmail.com>
2017-10-24 14:54:11 +0800

    Combines the separate validations into one validation and multiple list methods to a single list method.

--- core/src/main/java/com/ecc/hibernate_xml/service/PersonService.java
+++ core/src/main/java/com/ecc/hibernate_xml/service/PersonService.java
@@ -1,14 +1,12 @@
 package com.ecc.hibernate_xml.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import com.ecc.hibernate_xml.dao.PersonDao;
 import com.ecc.hibernate_xml.dto.PersonDTO;
 import com.ecc.hibernate_xml.assembler.PersonAssembler;
-import com.ecc.hibernate_xml.model.Name;
+import com.ecc.hibernate_xml.dao.PersonDao;
 import com.ecc.hibernate_xml.model.Person;
 import com.ecc.hibernate_xml.util.validator.ValidationException;
 import com.ecc.hibernate_xml.util.validator.ModelValidator;
@@ -26,15 +24,29 @@ public class PersonService extends AbstractService<Person, PersonDTO> {
 		validator = ModelValidator.create();
 	}
 
-	public String validateName(String data, String component) throws ValidationException {
+	public void validate(PersonDTO person) throws ValidationException {
+		validateName(person.getName().getTitle(), "Title");
+		validateName(person.getName().getLastName(), "Last name");
+		validateName(person.getName().getFirstName(), "First name");
+		validateName(person.getName().getMiddleName(), "Middle name");
+		validateName(person.getName().getSuffix(), "Suffix");
+		validateAddress(person.getAddress().getStreetNumber(), "Street number");
+		validateAddress(person.getAddress().getBarangay(), "Barangay");
+		validateAddress(person.getAddress().getMunicipality(), "Municipality");
+		validateAddress(person.getAddress().getZipCode(), "Zip code");
+		validateBirthday(person.getBirthday());
+		validateGWA(person.getGWA());
+		validateDateHired(person.getCurrentlyEmployed(), person.getDateHired());
+	}
+
+	private void validateName(String data, String component) throws ValidationException {
 		if (!component.equals("Title") && !component.equals("Suffix")) {
 			validator.validate("NotEmpty", data, component);		
 		}
 		validator.validate("MaxLength", data, DEFAULT_MAX_CHARACTERS, component);
-		return data;		
 	}
 
-	public <T> T validateAddress(T data, String component) throws ValidationException {
+	private <T> void validateAddress(T data, String component) throws ValidationException {
 		validator.validate("NotEmpty", data, component);
 
 		if (component.equals("Street number")) {
@@ -43,38 +55,28 @@ public class PersonService extends AbstractService<Person, PersonDTO> {
 		else if (component.equals("Municipality")) {
 			validator.validate("MaxLength", data, MAX_MUNICIPALITY_CHARACTERS, component);			
 		}
-		return data;
 	}
 
-	public BigDecimal validateGWA(BigDecimal GWA) throws ValidationException {
+	private void validateBirthday(Date birthday) throws ValidationException {
+		validator.validate("NotEmpty", birthday, "Birthday");
+	}
+
+	private void validateGWA(BigDecimal GWA) throws ValidationException {
+		validator.validate("NotEmpty", GWA, "GWA");
 		validator.validate("Minimum", GWA, 1, "GWA");
 		validator.validate("Maximum", GWA, 5, "GWA");
-		return GWA;
 	}
 
-	public Date validateDateHired(Boolean currentlyEmployed, Date dateHired) throws ValidationException {
+	private void validateDateHired(Boolean currentlyEmployed, Date dateHired) throws ValidationException {
 		if (!currentlyEmployed && dateHired != null) {
 			throw new ValidationException("Date hired cannot be assigned if person is unemployed.");
 		}
 		else if (currentlyEmployed && dateHired == null) {
-			throw new ValidationException("Date hired cannot be null if person is employed.");
+			throw new ValidationException("Date hired cannot be empty if person is employed.");
 		}
-		return dateHired;
 	}
 
-	public List<PersonDTO> listPersonsByGwa() {
-		return assembler.createDTO(personDao.list().stream().sorted((firstPerson, secondPerson) -> 
-			firstPerson.getGWA() == null? 1: 
-				secondPerson.getGWA() == null? -1:	
-					firstPerson.getGWA().compareTo(secondPerson.getGWA())
-		).collect(Collectors.toList()));
-	}
-
-	public List<PersonDTO> listPersonsByDateHired() {
-		return assembler.createDTO(personDao.listByDateHired());
-	}
-
-	public List<PersonDTO> listPersonsByLastName() {
-		return assembler.createDTO(personDao.listByLastName());
+	public List<PersonDTO> list(String lastName, Integer roleId, Date birthday, String orderBy, String order) {
+		return assembler.createDTO(personDao.list(lastName, roleId, birthday, orderBy, order));		
 	}
 }
