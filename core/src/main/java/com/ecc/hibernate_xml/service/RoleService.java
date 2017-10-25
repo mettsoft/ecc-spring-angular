745b6bcd3b3fcda5fcd991142bae461b38d0301c
Emmett Young <emmettyoung92@gmail.com>
2017-10-25 12:17:47 +0800

    Removes unused methods and imports in RoleService.

--- core/src/main/java/com/ecc/hibernate_xml/service/RoleService.java
+++ core/src/main/java/com/ecc/hibernate_xml/service/RoleService.java
@@ -1,15 +1,12 @@
 package com.ecc.hibernate_xml.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import com.ecc.hibernate_xml.dao.DaoException;
-import com.ecc.hibernate_xml.dao.RoleDao;
-import com.ecc.hibernate_xml.dao.PersonDao;
-import com.ecc.hibernate_xml.model.Role;
-import com.ecc.hibernate_xml.model.Person;
 import com.ecc.hibernate_xml.dto.RoleDTO;
 import com.ecc.hibernate_xml.dto.PersonDTO;
+import com.ecc.hibernate_xml.dao.RoleDao;
+import com.ecc.hibernate_xml.model.Role;
+import com.ecc.hibernate_xml.model.Person;
 import com.ecc.hibernate_xml.assembler.RoleAssembler;
 import com.ecc.hibernate_xml.assembler.PersonAssembler;
 import com.ecc.hibernate_xml.util.validator.ValidationException;
@@ -19,7 +16,6 @@ public class RoleService extends AbstractService<Role, RoleDTO> {
 	private static final Integer MAX_CHARACTERS = 20;
 
 	private final RoleDao roleDao;
-	private final PersonDao personDao;
 	private final ModelValidator validator;
 	private final PersonAssembler personAssembler;
 
