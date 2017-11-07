842a4bad07e583bdead7d210ea50414326546915
Emmett Young <emmettyoung92@gmail.com>
2017-11-07 08:29:31 +0800

    Removes unused method in NumberUtils.java

--- util/src/main/java/com/ecc/spring_xml/util/NumberUtils.java
+++ util/src/main/java/com/ecc/spring_xml/util/NumberUtils.java
@@ -1,21 +1,11 @@
 package com.ecc.spring_xml.util;
 
-import java.math.BigDecimal;
-
 public class NumberUtils {
 	public static Integer createInteger(String number) {
 		try {
 			return Integer.valueOf(number);
 		}
 		catch (NumberFormatException cause) {
-			return null;
-		}
- 	}
-	public static BigDecimal createBigDecimal(String number) {
-		try {
-			return new BigDecimal(number);
-		}
-		catch (NumberFormatException|NullPointerException cause) {
 			return null;
 		}
  	}
