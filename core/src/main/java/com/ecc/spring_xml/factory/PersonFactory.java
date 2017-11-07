27f682f22ad43bfd080a9d95fe8bc36e95c4633b
Emmett Young <emmettyoung92@gmail.com>
2017-11-07 08:16:46 +0800

    Localizes exception thrown by PersonFactory.

--- core/src/main/java/com/ecc/spring_xml/factory/PersonFactory.java
+++ core/src/main/java/com/ecc/spring_xml/factory/PersonFactory.java
@@ -1,19 +1,18 @@
 package com.ecc.spring_xml.factory;
 
-import java.io.IOException;
-
 import com.google.gson.Gson;
 import org.apache.commons.io.IOUtils;
 
 import com.ecc.spring_xml.dto.PersonDTO;
+import com.ecc.spring_xml.util.ValidationException;
 
 public class PersonFactory {
 	public static PersonDTO createPersonDTO(byte[] data) {
 		try {
-			return new Gson().fromJson(IOUtils.toString(data, "utf-8"), PersonDTO.class);		
+			return new Gson().fromJson(IOUtils.toString(data, "utf-8"), PersonDTO.class);
 		}
 		catch (Exception exception) {
-			throw new RuntimeException("Invalid file format!");
+			throw new ValidationException("person.validation.message.invalidFormat");
 		}
 	}
 }
