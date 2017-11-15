dfeddbfd29f6dd42a45d5ea3f0a49f63394ea147
Emmett Young <emmettyoung92@gmail.com>
2017-11-15 19:35:14 +0800

    Removes duplicated User entries in Users module.

--- core/src/main/java/com/ecc/spring_security/dao/UserDao.java
+++ core/src/main/java/com/ecc/spring_security/dao/UserDao.java
@@ -3,9 +3,6 @@ package com.ecc.spring_security.dao;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.criterion.CriteriaSpecification;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +17,7 @@ public class UserDao extends AbstractDao<User> {
 	@Transactional
 	public List<User> list() {
 		return sessionFactory.getCurrentSession()
-			.createCriteria(User.class)
-			.createAlias("permissions", "P", CriteriaSpecification.LEFT_JOIN)
-			.add(
-				Restrictions.or(
-					Restrictions.ne("P.name", "ROLE_ADMIN"),
-					Restrictions.isNull("P.name")
-				)
-			)
-			.addOrder(Order.asc("id"))
+			.createQuery("SELECT U FROM User U LEFT JOIN U.permissions P WHERE P.name <> 'ROLE_ADMIN' OR P.name IS NULL GROUP BY U.id ORDER BY U.id")
 			.list();
 	}
 
