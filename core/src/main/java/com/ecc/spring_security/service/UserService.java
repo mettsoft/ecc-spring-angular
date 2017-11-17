47096d96acd2a7549b2274a68daead484e703527
Emmett Young <emmettyoung92@gmail.com>
2017-11-17 08:51:43 +0800

    Adds non empty validation to password.

--- core/src/main/java/com/ecc/spring_security/service/UserService.java
+++ core/src/main/java/com/ecc/spring_security/service/UserService.java
@@ -58,6 +58,7 @@ public class UserService extends AbstractService<User, UserDTO> implements Valid
     	UserDTO user = (UserDTO) command;
 		ValidationUtils.testNotEmpty(user.getUsername(), "username", errors, "localize:user.data.column.username");
 		ValidationUtils.testMaxLength(user.getUsername(), "username", errors, MAX_CHARACTERS, "localize:user.data.column.username");
+		ValidationUtils.testNotEmpty(user.getPassword(), "password", errors, "localize:user.form.label.password");
     }
 
 	@Override
