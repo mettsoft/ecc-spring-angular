89a52d3941cec8431e218d15565a4fccbed01376
Emmett Young <emmettyoung92@gmail.com>
2017-11-19 19:45:19 +0800

    Completes the basic role management module.

--- web/src/main/webapp/components/role_management/role_management.component.js
+++ web/src/main/webapp/components/role_management/role_management.component.js
@@ -1,18 +1,52 @@
 angular.module('roleManagement', [])
   .component('roleManagement', {
     templateUrl: './components/role_management/role_management.template.html',
-    controller: ['$scope', '$rootScope', function($scope, $rootScope) {
+    controller: ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+      $http.get('/roles').then(response => this.data = response.data);
       $scope.__proto__ = $rootScope;
-      this.headerTitle = 'Header Title';
-      this.errorMessages = ['Foo', 'Bar'];
-      this.successMessage = 'Hello World';
-      this.writeEndpoint = '/create';
-      this.data = [{name: 'hello'}];
-      this.deleteRow = role => {
+      this.command = {};
+
+      this.editRow = index => this.command = Object.assign({index: index}, this.data[index]);
+
+      this.deleteRow = index => {
+        let role = this.data[index];
         if (confirm($scope.tr('role.data.form.button.deleteConfirmation', role.name))) {
-          alert(`${role.name} is deleted!`);
+          $http.delete(`/roles/${role.id}`).then(response => {
+            this.data.splice(index, 1);
+            this.errorMessages = null;
+            this.successMessage = $scope.tr('role.successMessage.delete', role.name);
+          }).catch(response => {
+            this.errorMessages = response.data.errors;
+            this.successMessage = null;
+          });
         }
       };
-      this.editRow = role => alert(`${role.name} is edited!`);
+
+      this.onCommandSubmit = command => {
+        let method = command.id? 'PUT': 'POST'; 
+        $http({
+          method: method,
+          url: '/roles',
+          data: command
+        }).then(response => {
+          this.command = {};
+          this.errorMessages = null;
+          if (!command.id) {
+            this.successMessage = $scope.tr('role.successMessage.create', command.name);
+            this.data.push(response.data);
+          }
+          else {
+            this.data[command.index] = response.data;
+            this.successMessage = $scope.tr('role.successMessage.update', command.name);
+            if (command.persons.length > 0) {
+              let personNames = command.persons.map(p => [p.name.title, p.name.lastName + ',', p.name.firstName, p.name.middleName, p.name.suffix].join(' ').trim());
+              this.successMessage += ` ${$scope.tr('role.successMessage.affectedPersons', personNames.join(';'))}`;
+            }
+          }
+        }).catch(response => {
+          this.errorMessages = response.data.errors;
+          this.successMessage = null;
+        });
+      };
     }]
   });
