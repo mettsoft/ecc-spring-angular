be47fcec569d9c45678cfe33ad5f354e4b2340f2
Emmett Young <emmettyoung92@gmail.com>
2017-11-19 14:55:26 +0800

    Adds role form.

--- web/src/main/webapp/components/role_management/role_management.component.js
+++ web/src/main/webapp/components/role_management/role_management.component.js
@@ -1,8 +1,11 @@
 angular.module('roleManagement', [])
   .component('roleManagement', {
     templateUrl: './components/role_management/role_management.template.html',
-    controller: [function() {
-        this.errorMessages = ['Foo', 'Bar'];
-        this.successMessage = 'Hello World';
+    controller: ['$scope', '$rootScope', function($scope, $rootScope) {
+      $scope.__proto__ = $rootScope;
+      this.headerTitle = 'Header Title';
+      this.errorMessages = ['Foo', 'Bar'];
+      this.successMessage = 'Hello World';
+      this.writeEndpoint = '/create';
     }]
   });
