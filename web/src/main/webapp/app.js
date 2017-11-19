bc94d45a2289fba7e46e06c88231f1dfee41ba80
Emmett Young <emmettyoung92@gmail.com>
2017-11-19 17:38:37 +0800

    Add argument replacer in tr() for localizing text.

--- web/src/main/webapp/app.js
+++ web/src/main/webapp/app.js
@@ -1,11 +1,12 @@
 angular.module('personRegistrationSystem', ['roleManagement'])
 	.run(['$rootScope', '$http', function($rootScope, $http) {
 		$rootScope.locale = 'en';
-		$http({
-			method: 'GET',
-			url: '/resources'
-		}).then(response => {
+		$http.get('/resources').then(response => {
 			$rootScope.dictionary = response.data;
-			$rootScope.tr = key => $rootScope.dictionary[$rootScope.locale][key];
+			$rootScope.tr = (key, ...args) => {
+				let text = $rootScope.dictionary[$rootScope.locale][key];
+				args.forEach((value, index) => text = text.replace(`{${index}}`, value));
+				return text;
+			};
 		});
 	}]);
