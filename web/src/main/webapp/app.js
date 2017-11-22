91ea34f9b5de5cf71f031b49ed8efc909f71739e
Emmett Young <emmettyoung92@gmail.com>
2017-11-22 16:04:11 +0800

    Fixes the unavailable translation on application start-up.

--- web/src/main/webapp/app.js
+++ web/src/main/webapp/app.js
@@ -1,19 +1,26 @@
-let externalDependencies = ['ui.router', 'base64', 'ngCookies'];
-let internalDependencies = ['roleManagement', 'userManagement', 'login', 'Authentication'];
+var TRANSLATIONS;
+
+$.ajax({
+  dataType: "json",
+  url: "/resources",
+  async: false,
+  success: data => TRANSLATIONS = data
+});
+
+const externalDependencies = ['ui.router', 'base64', 'ngCookies'];
+const internalDependencies = ['roleManagement', 'userManagement', 'login', 'Authentication'];
 
 angular.module('personRegistrationSystem', [...externalDependencies, ...internalDependencies])
 	.run(['$rootScope', '$http', '$state', '$transitions', 'Authentication',
 		function($rootScope, $http, $state, $transitions, Authentication) {
 			// Expose convenience methods and objects to $rootScope.
 			$rootScope.locale = 'en';
-			$http.get('/resources').then(response => {
-				let dictionary = response.data;
-				$rootScope.tr = (key, ...args) => {
-					let text = dictionary[$rootScope.locale][key];
-					args.forEach((value, index) => text = text.replace(`{${index}}`, value));
-					return text;
-				};
-			});
+			$rootScope.tr = (key, ...args) => {
+				let text = TRANSLATIONS[$rootScope.locale][key];
+				args.forEach((value, index) => text = text.replace(`{${index}}`, value));
+				return text;
+			};
+
 			$rootScope.access = Authentication.access;
 			$rootScope.$state = $state;			
 
